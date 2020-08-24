b032d6e9e1e909c5b2493ae81de88c802689e952
Jesulen Vicente <vicente.jesulen@gmail.com>
2020-08-24 20:00:29 -0300

    Condicionais

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * @description variaveis
  */
 
+
+/*  
  var name = 'Jesulen'
  var age = 24
 
@@ -13,6 +15,60 @@
  //const age  = 24 
 
  console.log('Name: ', name)
- console.log('Age: ', age)
+ console.log('Age: ', age) 
+ */
+ 
+
+/**
+ * @description Operações lógicas e booleanas
+ */
+
+ /* 
+ let ano, idade
+ ano = 2020
+ idade = 24
+ const nascimento = ano - idade
+ console.log('Jesulen nasceu em ', nascimento)
+
+ let likePacoca, hasPacoca
+ likePacoca = true
+ hasPacoca = false
+ let pacocaServida = likePacoca && hasPacoca
+ console.log('Pacoca servida?', pacocaServida) 
+ */
+
+ /**
+  * @description if/else, ternário, switch, valores falsos e verdadeiros
+  */
+
+ let likePacoca, hasPacoca
+ likePacoca = true
+ hasPacoca = true
 
 
+if (likePacoca) {
+    console.log('certo')
+} else if (likePacoca && hasPacoca) {
+    console.log('Feliz')
+} else {
+    HTMLFormControlsCollection.log('Triste')
+}
+
+let servido = hasPacoca ? 'Sim, servido' : 'Nao, nao servido'
+console.log(servido)
+
+let idadeJesulen = 23
+let idadeThami = 24
+const jesulenMaisVelho = idadeJesulen > idadeThami
+
+switch(jesulenMaisVelho){
+    case true:
+        console.log('Jesulen e mais velho')
+        break
+    case false:
+        console.log('Thami e mais veha')
+        break
+    default:
+        console.log('Indeterminado')
+        break
+}
