af253212e2a78f926ae5dc3bbded3750e55c076b
Jesulen Vicente <vicente.jesulen@gmail.com>
2020-08-24 19:22:55 -0300

    commit variaveis

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+/**
+ * @description variaveis
+ */
+
+ var name = 'Jesulen'
+ var age = 24
+
+ //let name = 'Jesulen'
+ //let age = 24
+
+ // const só pode ser atribuído valor uma vez, no momento da criação
+ //const name = 'Jesulen'
+ //const age  = 24 
+
+ console.log('Name: ', name)
+ console.log('Age: ', age)
+
+
